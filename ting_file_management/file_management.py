ab565bf8921218a1dbe2dfd4cc2b5ee2b5a4e11a
MarcusVMendes <marcus.mendes89@gmail.com>
2022-05-12 01:36:15 -0300

    Requisito 2 concluido

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    else:
+        try:
+            with open(path_file) as file:
+                return [item.strip("\n") for item in file]
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
