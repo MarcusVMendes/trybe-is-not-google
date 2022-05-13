522a3bacaf333fb1407f4d004129e0c54c885efb
MarcusVMendes <marcus.mendes89@gmail.com>
2022-05-12 21:11:54 -0300

    Requisito 6 concluido

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    for item in instance.data:
+        with open(item) as file:
+            lines = file.read().split("\n")
+            result.append({
+                "palavra": word,
+                "arquivo": item,
+                "ocorrencias": [],
+            })
+            for index, line in enumerate(lines):
+                if word.lower() in line.lower():
+                    result[-1]["ocorrencias"].append({
+                        "linha": index + 1,
+                    })
+            if result[-1]["ocorrencias"] == []:
+                result.pop()
+    return result
 
 
 def search_by_word(word, instance):
